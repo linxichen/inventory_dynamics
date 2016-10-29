11ff030e31a7d9604f99f095d23cf7356f10573e
Linxi Chen <empirechen88@gmail.com>
2016-10-28 22:11:53 -0400

    Done with the rejection table, but the result doesn't make sense yet

--- empirics/agg_empirics/codes/STEP4_skewness_montecarlo.m
+++ empirics/agg_empirics/codes/STEP4_skewness_montecarlo.m
@@ -9,86 +9,177 @@ clc;
 addpath(genpath('helpers'));
 
 %% specify parameters of experiment
-nsize = [124 224];
 nseries = 2500;
-small_size = 1;
-large_size = 2;
-normal = 1;
-lognormal_low = 2; % 
-lognormal_high = 3;
-weibull_low = 4;
-weibull_high = 5;
-gamma_low = 6;
-gamma_high = 7;
+
+% list of filters
+filters = {'hp1600','hp1e5','bp6_32','bp2_80','fd'};
+for i_filter = 1:length(filters)
+	expr = strcat(filters{i_filter},'=',num2str(i_filter),';');
+	eval(expr);
+end
+nfilters = length(filters);
+
+% sample sizes
+sample_sizes =  [124,224];
+nsizes = length(sample_sizes);
+
+% set of innovations
+innovations = {'normal',...
+	           'lognormal_low',...
+			   'lognormal_high',...
+			   'weibull_low',...
+			   'weibull_high',...
+			   'gamma_low',...
+			   'gamma_high',...
+			   };
+for i_innov = 1:length(innovations)
+	expr = strcat(innovations{i_innov},'=',num2str(i_innov),';');
+	eval(expr);
+end
+ninnov = length(innovations);
+
+
+%
 sim_obs = cell(7,2); % for innovations, two sample sizes
 sim_cyc = cell(7,2);
 
-%% log-normal play ground
-
-%% generate data for one test
-i_sample_length = 1;
-i_innov = 1;
-
-sample_length = nsize(i_sample_length);
-switch i_innov % formulas are from Wiki
-	case normal
-		cyc_u = randn(sample_length,nseries);
-	case lognormal_low
-		target_skewness = 0.95;
-		myfun = @(s) target_skewness - (exp(s)+2)*sqrt(exp(s)-1);
-		ssigmasq = fsolve(myfun,1);
-		sample = lognrnd(0,sqrt(ssigmasq),sample_length,nseries);
-		transformed_sample = (sample-mean(sample))/(std(sample)); % force 0 mean, 1 std
-		cyc_u = transformed_sample;
-	case lognormal_high
-		target_skewness = 6.2;
-		myfun = @(s) target_skewness - (exp(s)+2)*sqrt(exp(s)-1);
-		ssigmasq = fsolve(myfun,1);
-		sample = lognrnd(0,sqrt(ssigmasq),sample_length,nseries);
-		transformed_sample = (sample-mean(sample))/(std(sample)); % force 0 mean, 1 std
-		cyc_u = transformed_sample;
-	case weibull_low
-		% #stopped here
-		target_skewness = 0.95;
-		k = 0.5;
-		mmufn = @(l) l*gamma(1+1/k);
-		ssigmafn = @(l) sqrt( l^2*( gamma(1+2/k)-(gamma(1+1/k))^2 ));
-		ttaufn = @(l) (gamma(1+3/k)*l^3-3*mmufn(l)*(ssigmafn(l)^2)-mmufn(l)^3)...
-			/ (ssigmafn(l)^3) - target_skewness;
-		llambda = fsolve(ttaufn,10);
-		cyc_u = wblrnd(llambda,k,1e7,1);
+%% generate data for all innovations and sample lengths
+for i_innov = 1:7
+	for i_sample_length = 1:2
+		
+		sample_length = sample_sizes(i_sample_length);
+		switch i_innov % formulas are from Wiki
+			case normal
+				cyc_u = randn(sample_length,nseries);
+			case lognormal_low
+				target_skewness = 0.95;
+				myfun = @(s) target_skewness - (exp(s)+2)*sqrt(exp(s)-1);
+				ssigmasq = fsolve(myfun,1);
+				sample = lognrnd(0,sqrt(ssigmasq),sample_length,nseries);
+				transformed_sample = zscore(sample); % force 0 mean, 1 std
+				cyc_u = transformed_sample;
+			case lognormal_high
+				target_skewness = 6.2;
+				myfun = @(s) target_skewness - (exp(s)+2)*sqrt(exp(s)-1);
+				ssigmasq = fsolve(myfun,1);
+				sample = lognrnd(0,sqrt(ssigmasq),sample_length,nseries);
+				transformed_sample = zscore(sample); % force 0 mean, 1 std
+				cyc_u = transformed_sample;
+			case weibull_low
+				% Note that skewness depend on k only!!
+				target_skewness = 0.95;
+				part1 = @(k) (gamma(1+2/k)-gamma(1+1/k)^2)^(-3/2);
+				part2 = @(k) gamma(1+3/k)-3*gamma(1+1/k)*(gamma(1+2/k)-gamma(1+1/k)^2)...
+					-gamma(1+1/k)^3;
+				ttaufn = @(k) part1(k)*part2(k) - target_skewness;
+				k = fsolve(ttaufn,1.5);
+				llambda = sqrt( 1/( gamma(1+2/k)-gamma(1+1/k)^2 ) );
+				sample = wblrnd(llambda,k,sample_length,nseries);
+				cyc_u =  zscore(sample); % force 0mean 1variance
+			case weibull_high
+				% Note that skewness depend on k only!!
+				target_skewness = 6.2;
+				part1 = @(k) (gamma(1+2/k)-gamma(1+1/k)^2)^(-3/2);
+				part2 = @(k) gamma(1+3/k)-3*gamma(1+1/k)*(gamma(1+2/k)-gamma(1+1/k)^2)...
+					-gamma(1+1/k)^3;
+				ttaufn = @(k) part1(k)*part2(k) - target_skewness;
+				k = fsolve(ttaufn,1.5);
+				llambda = sqrt( 1/( gamma(1+2/k)-gamma(1+1/k)^2 ) );
+				sample = wblrnd(llambda,k,sample_length,nseries);
+				cyc_u =  zscore(sample); % force 0mean 1variance
+			case gamma_low
+				target_skewness = 0.95;
+				k = (2/target_skewness)^2;
+				ttheta = sqrt(1/k);
+				sample = gamrnd(k,ttheta,sample_length,nseries);
+				cyc_u =  zscore(sample); % force 0mean 1variance
+			case gamma_high
+				target_skewness = 6.2;
+				k = (2/target_skewness)^2;
+				ttheta = sqrt(1/k);
+				sample = gamrnd(k,ttheta,sample_length,nseries);
+				cyc_u =  zscore(sample); % force 0mean 1variance				
+		end
+		
+		trend_u = randn(sample_length,nseries);
+		trend = zeros(sample_length,1);
+		cyc = zeros(sample_length,1);
+		obs_table = zeros(sample_length,nseries);
+		cyc_table = obs_table;
+		for i_series = 1:nseries
+			trend(1) = 0 + 0.01 + 0.01*trend_u(1);
+			cyc(1) = 1.5*0 - 0.58*0 + 0.01*cyc_u(1);
+			cyc(2) = 1.5*cyc(1) - 0.58*0 + 0.01*cyc_u(2);
+			for t = 2:length(trend)
+				trend(t) = trend(t-1) + 0.01 + 0.01*trend_u(t);
+				if t >= 3
+					cyc(t) = 1.5*cyc(t-1) - 0.58*cyc(t-2) + 0.01*cyc_u(t);
+				end
+			end
+			
+			% store the observable and cyclical part
+			obs_table(:,i_series) = trend+cyc;
+			cyc_table(:,i_series) = cyc;
+		end
+		sim_obs{i_innov,i_sample_length} = obs_table;
+		sim_cyc{i_innov,i_sample_length} = cyc_table;
+	end
 end
 
-trend_u = randn(sample_length,nseries);
-trend = zeros(sample_length,1);
-cyc = zeros(sample_length,1);
-obs_table = zeros(sample_length,nseries);
-cyc_table = obs_table;
-for i_series = 1:nseries
-	trend(1) = 0 + 0.01 + 0.01*trend_u(1);
-	cyc(1) = 1.5*0 - 0.58*0 + 0.01*cyc_u(1);
-	cyc(2) = 1.5*cyc(1) - 0.58*0 + 0.01*cyc_u(2);
-	for t = 2:length(trend)
-		trend(t) = trend(t-1) + 0.01 + 0.01*trend_u(t);
-		if t >= 3
-			cyc(t) = 1.5*cyc(t-1) - 0.58*cyc(t-2) + 0.01*cyc_u(t);
+%% For each data series
+rej_obs_table = cell(nfilters,ninnov,length(sample_sizes));
+rej_cyc_table = cell(nfilters,ninnov,length(sample_sizes));
+
+for i_innov = 1:ninnov
+	for i_sample_length = 1:length(sample_sizes)
+		raw_obs = sim_obs{i_innov,i_sample_length};
+		raw_cyc = sim_cyc{i_innov,i_sample_length};
+		for i_filter = 1:nfilters
+			rej_obs_count = 0;
+			rej_cyc_count = 0;
+			for i_series = 1:nseries
+				% apply filter by case
+				switch i_filter
+					case hp1600
+						[~,detrended_obs] = hpfilter(raw_obs(:,i_series),1600);
+						[~,detrended_cyc] = hpfilter(raw_cyc(:,i_series),1600);
+					case hp1e5
+						[~,detrended_obs] = hpfilter(raw_obs(:,i_series),1e5);
+						[~,detrended_cyc] = hpfilter(raw_cyc(:,i_series),1e5);
+					case bp6_32
+						detrended_obs = bandpass(raw_obs(:,i_series),6,32);
+						detrended_cyc = bandpass(raw_cyc(:,i_series),6,32);
+					case bp2_80
+						detrended_obs = bandpass(raw_obs(:,i_series),2,80);
+						detrended_cyc = bandpass(raw_cyc(:,i_series),2,80);	
+					case fd
+						detrended_obs = diff(raw_obs(:,i_series));
+						detrended_cyc = diff(raw_cyc(:,i_series));
+				end
+				
+				% compute skewness and test
+				[s_obs,t_obs] = skewTestAC(detrended_obs);
+				[s_cyc,t_cyc] = skewTestAC(detrended_cyc);
+				% skewCoeffs = s;
+				if pnorm(abs(t_obs)) > 0.95
+					rej_obs_count = rej_obs_count + 1;
+				end
+				if pnorm(abs(t_cyc)) > 0.95
+					rej_cyc_count = rej_cyc_count + 1;
+				end
+				% # stopped here
+			end
+			rej_obs_table{i_filter,i_innov,i_sample_length}=rej_obs_count/nseries;
+			rej_cyc_table{i_filter,i_innov,i_sample_length}=rej_cyc_count/nseries;
 		end
 	end
-	
-	% store the observable and cyclical part
-	obs_table(:,i_series) = trend+cyc;
-	cyc_table(:,i_series) = cyc;
 end
-sim_obs{i_innov,i_sample_length} = obs_table;
-sim_cyc{i_innov,i_sample_length} = cyc_table;
-
 
 %% computes skewness for each variable
 %  for ISratio level
 format bank
 disp('========== IS ratio =============');
-raw = log(rISratio);
-[~,HP1] = hpfilter(raw,1600);
+
 [~,HP2] = hpfilter(raw,1e5);
 BP1 = bandpass(raw,6,32);
 BP2 = bandpass(raw,2,80);
