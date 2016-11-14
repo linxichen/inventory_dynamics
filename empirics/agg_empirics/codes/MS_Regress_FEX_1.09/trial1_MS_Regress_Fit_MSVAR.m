f4d98435170ff1d4bf009e3e0c2693b45f35d76c
Linxi Chen <linxichen@gmail.com>
2016-11-14 17:07:33 -0500

    some changes, nothing significant on the MSVAR side

--- empirics/agg_empirics/codes/MS_Regress_FEX_1.09/trial1_MS_Regress_Fit_MSVAR.m
+++ empirics/agg_empirics/codes/MS_Regress_FEX_1.09/trial1_MS_Regress_Fit_MSVAR.m
@@ -7,20 +7,25 @@ addpath('data_Files');
 
 imported=importdata('./data_Files/trial1.txt');  % load some Data.
 data = imported.data;
-data = diff(data);
+%data = 100*diff(data(:,2:3));
+shift = 3;
+forward_resid_rCIPI = data(130+2+shift:end,2);
+match_GDP = data(130+1:end-shift,3);
+data = 100*[forward_resid_rCIPI diff(match_GDP)];
 
 dep=data;                  % Defining dependent variables in system
-nLag=2;                             % Number of lags in system
+nLag=1;                             % Number of lags in system
 k=2;                                % Number of States
 doIntercept=1;                      % add intercept to equations?
 advOpt.distrib='Normal';            % The Distribution assumption (only 'Normal' for MS VAR models)
 advOpt.std_method=2;                % Defining the method for calculation of standard errors. See pdf file for more details
 advOpt.diagCovMat=0;                % since it reduced form, diagonal is stupid
-advOpt.useMex=1;
+advOpt.useMex=1;                % uses mex version of hamilton filter
+advOpt.optimizer='fmincon';     % use fmincon instead
 
 [Spec_Out]=MS_VAR_Fit(dep,nLag,k,doIntercept,advOpt);
 
-save trials3.mat
+save trials1.mat
 
 rmpath('m_Files');
 rmpath('data_Files'); 
