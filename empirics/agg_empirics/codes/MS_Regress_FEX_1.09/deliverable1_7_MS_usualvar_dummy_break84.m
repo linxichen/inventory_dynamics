3bbce980dbbf544e7244237c664e1067994a0051
Linxi Chen <empirechen88@gmail.com>
2016-12-05 20:05:26 -0500

    created the discrete simulation and used 4 lags

--- empirics/agg_empirics/codes/MS_Regress_FEX_1.09/deliverable1_7_MS_usualvar_dummy_break84.m
+++ empirics/agg_empirics/codes/MS_Regress_FEX_1.09/deliverable1_7_MS_usualvar_dummy_break84.m
@@ -76,7 +76,7 @@ recessband = recessionplot;
 %% collect residual as z
 rec_flag = step1_Spec_Out.smoothProb(:,2) > 0.7;
 z = step1_Spec_Out.resid;
-lags = 3;
+lags = 4;
 zlag = lagmatrix(z,1:lags);
 zlag_safe = zlag(lags+1:end,:);
 z_safe = z(lags+1:end,:);
@@ -217,8 +217,91 @@ subplot(1,2,2)
 plot(turbulent(3,:)')
 ylim([-0.05 0.15])
 
-%% Discrete simulation
+%% Discrete simulation for 1981 recession
+Pphi_exp = reshape(pphi_exp,nvar,nvar,lags);
+Pphi_rec = reshape(pphi_rec,nvar,nvar,lags);
+peakdate = 130; % 1981:III
+troughdate = 135; % 1982:IV
+ylags_demean = zeros(nvar,lags);
+ysim_demean = zeros(nvar,troughdate-peakdate);
+exp_mean(1,1) = step1_Spec_Out.Coeff.S_Param{1,1}(1,1);
+rec_mean(1,1) = step1_Spec_Out.Coeff.S_Param{1,1}(2,1);
+exp_mean(2,1) = step1_Spec_Out.Coeff.S_Param{1,2}(1,1);
+rec_mean(2,1) = step1_Spec_Out.Coeff.S_Param{1,2}(2,1);
+exp_mean(3,1) = step1_Spec_Out.Coeff.S_Param{1,3}(1,1);
+rec_mean(3,1) = step1_Spec_Out.Coeff.S_Param{1,3}(2,1);
 
+for i_lag = 1:lags
+	ylags_demean(:,i_lag) = data(peakdate-i_lag+1,:)'-exp_mean;
+end
+ysim_demean(:,1) =  Pphi_rec(:,:,1)*ylags_demean(:,1) ...
+	+ Pphi_rec(:,:,2)*ylags_demean(:,2) ...
+	+ Pphi_rec(:,:,3)*ylags_demean(:,3) ...
+	+ Pphi_rec(:,:,4)*ylags_demean(:,4);
+for date = 2:troughdate-peakdate
+	ylags_demean(:,4) = ylags_demean(:,3);
+	ylags_demean(:,3) = ylags_demean(:,2);
+	ylags_demean(:,2) = ylags_demean(:,1);
+	ylags_demean(:,1) = ysim_demean(:,date-1); 
+	ysim_demean(:,date) = Pphi_rec(:,:,1)*ylags_demean(:,1) ...
+	+ Pphi_rec(:,:,2)*ylags_demean(:,2) ...
+	+ Pphi_rec(:,:,3)*ylags_demean(:,3) ...
+	+ Pphi_rec(:,:,4)*ylags_demean(:,4);
+end
+ysim = ysim_demean + repmat(rec_mean,1,troughdate-peakdate);
+
+data_seq = [data(peakdate:troughdate,:)]';
+sim_seq = [data(peakdate,:)',ysim];
+
+figure
+subplot(3,1,1)
+plot(0:troughdate-peakdate,[data(peakdate:troughdate,1)'],...
+	0:troughdate-peakdate,[data(peakdate,1)' ysim(1,:)],'r--','LineWidth',lw,'MarkerSize',msz)
+title('Sales Growth')
+subplot(3,1,2)
+plot(0:troughdate-peakdate,[data(peakdate:troughdate,2)'],...
+	0:troughdate-peakdate,[data(peakdate,2)' ysim(2,:)],'r--','LineWidth',lw,'MarkerSize',msz)
+ylabel('Percent')
+title('GDP Growth')
+subplot(3,1,3)
+plot(0:troughdate-peakdate,[data(peakdate:troughdate,3)'],...
+	0:troughdate-peakdate,[data(peakdate,3)' ysim(3,:)],'r--','LineWidth',lw,'MarkerSize',msz)
+xlabel('Periods From Peak')
+title('CIPI/GDP^{Pot}')
+legend('Data','Discrete Shock')
+print('../../figures/discrete_demeaned','-depsc2','-r300');
+
+% revert to back to level
+data_level = [rSalesGoods(peakdate+1:troughdate+1) rGDP(peakdate+1:troughdate+1) rCIPI(peakdate+1:troughdate+1)]';
+sim_level = zeros(3,6);
+sim_level(:,1) = data_level(:,1);
+for date = 2:6
+	sim_level(1:2,date) = (1+sim_seq(1:2,date)/100).*sim_level(1:2,date-1);
+	sim_level(3,date) = sim_seq(3,date)/100.*GDPPOT(peakdate+1+date-1);
+end
+
+figure
+subplot(3,1,1)
+plot(0:troughdate-peakdate,data_level(1,:),...
+	0:troughdate-peakdate,sim_level(1,:),'r--','LineWidth',lw,'MarkerSize',msz)
+xticks(0:troughdate-peakdate)
+title('Sales Level')
+
+subplot(3,1,2)
+plot(0:troughdate-peakdate,data_level(2,:),...
+	0:troughdate-peakdate,sim_level(2,:),'r--','LineWidth',lw,'MarkerSize',msz)
+xticks(0:troughdate-peakdate)
+ylabel('Billions of 2009 Dollar')
+title('GDP Level')
+
+subplot(3,1,3)
+plot(0:troughdate-peakdate,data_level(3,:),...
+	0:troughdate-peakdate,sim_level(3,:),'r--','LineWidth',lw,'MarkerSize',msz)
+xticks(0:troughdate-peakdate)
+xlabel('Periods From Peak')
+title('CIPI Level')
+legend('Data','Discrete Shock')
+print('../../figures/discrete_level','-depsc2','-r300');
 
 
 %%
